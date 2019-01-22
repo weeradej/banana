ada72e8872c2b36cdecd3c0478ccdf5de7fd93f5
Weeradej <weeradejs@gmail.com>
2019-01-22 13:57:51 +0700

    commit master

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World.") //Pring message to console.
+	fmt.Println("Hello World.")       //Pring message to console.
+	fmt.Println("Hello World Again.") //Pring message to console.
 }
